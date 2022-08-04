b7d69e739fc8b264e53a3502e784bea5dfcc1b3e
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-04 14:55:01 -0300

    Preparando revisão de código

--- schemas/newSafeNoteSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-import { CreateSafeNote } from "../repositories/safeNoteRepository.js";
-
-export const newSafeNoteSchema = joi.object<CreateSafeNote>({
-    title: joi.string().max(50).required(),
-    text: joi.string().max(1000).required()
-});
--- server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./index.js";
-
-import dotenv from "dotenv";
-dotenv.config();
-
-const port = +process.env.PORT || 5001;
-app.listen(port, () => console.log(`Server live at port: ${port}`));
